fd26d76803f7de571929cf0b0d5ac864bb152e09
johnny1864 <johnny1864@yahoo.com>
2018-12-26 14:27:03 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
+import { AppComponent } from "./app.component";
+import { AboutComponent } from "./about/about.component";
+import { HomeComponent } from "./home/home.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, AboutComponent, HomeComponent, SidebarComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
